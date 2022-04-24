540ba617e6cf05b2fae7e9b72ba437ac6e17c108
ghedin-alison <alison.ghedin@outlook.com>
2022-04-24 19:16:13 -0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -10,3 +11,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
+
+ImportError: cannot import name 'escape' from 'jinja2' (/usr/local/lib/python3.9/site-packages/jinja2/__init__.py)
+
